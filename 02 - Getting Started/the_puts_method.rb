fa5c18f8100c1780d645efe119d0feba557c2986
s403o <eslam.adel.me@gmail.com>
2023-03-08 10:07:19 +0200

    puts method ✓

---
+++ 02 - Getting Started/the_puts_method.rb
@@ -0,0 +1,19 @@
+# new line added automatic
+1 + 1 # ruby still reading this line but didin't do any thing next 
+puts 1
+puts 1 + 1
+puts (1 + 1)
+
+puts "Hello"
+puts
+puts "world"
+
+puts 1
+puts 2
+puts 3
+
+puts 
+
+puts 1, 2, 3 # the comma will ignore the break line
+
+puts 1 * "hi" # type error
