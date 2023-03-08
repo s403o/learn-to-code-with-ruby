c542ab5ed62862465bf7d3c8d83338feccb73061
s403o <eslam.adel.me@gmail.com>
2023-03-08 20:37:58 +0200

    basic arith ✓

---
+++ 02 - Getting Started/basic_arithmetic_in_ruby.rb
@@ -0,0 +1,17 @@
+puts 1 + 5
+puts -6 + 10
+puts 3 * 5
+puts 3 * 4 * 5
+
+# PEMDAS
+
+puts 2 + 3 * 1
+puts 10 / 5
+
+puts 12.0 / 5 # 2.4
+puts 12 / 5.0 # 2.4
+
+puts 0.5 / 2 # 0.25
+
+puts 5 % 2
+puts 7 % 2
