ce65fcf58335e17f0fc93e6f1097144b60fef15a
s403o <eslam.adel.me@gmail.com>
2023-03-12 12:49:39 +0200

    declaring var ✓

---
+++ 03 - Variables/declaring_variables.rb
@@ -0,0 +1,14 @@
+age = 24
+puts age
+
+first_name = "Eslam"
+last_name = "Adel"
+
+puts first_name + last_name
+puts age + 5
+
+# we can assign to different data types without errors (note)
+val = 1
+val = "1"
+val = 1.1
+puts val
