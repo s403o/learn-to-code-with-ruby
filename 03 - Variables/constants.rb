0673c8ea2b7811413143e8a718dae89720a57791
s403o <eslam.adel.me@gmail.com>
2023-03-12 14:21:47 +0200

    constants ✓

---
+++ 03 - Variables/constants.rb
@@ -0,0 +1,9 @@
+PI = 3.14159
+
+TAX_RATE = 0.07
+
+puts PI
+puts TAX_RATE
+
+TAX_RATE = 0.13 # warning: already initialized as constant but you can do it in Ruby(Ruby trust you :"D")!
+puts TAX_RATE
